c8f76b53690490000a96eaaa8a6300c8e670223a
Yogada Bhalerao <148211801+Yogab15@users.noreply.github.com>
2024-06-04 12:35:31 +0530

    Update circleacobj.java (lab 10)

--- circleacobj.java
+++ circleacobj.java
@@ -1,6 +1,6 @@
 //Write a Java program to create a class called "Circle" with a radius attribute. You can access and modify this attribute.
 //Calculate the area and circumference of the circle.
-
+ 
 public class circleacobj {
     private double radius;  // Declares a private instance variable 'radius' to store the radius of the circle
     
