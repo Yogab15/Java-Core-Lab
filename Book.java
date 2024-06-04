b031941d0ece6cdabdddc47c4a29ef535bdeced3
Yogada Bhalerao <148211801+Yogab15@users.noreply.github.com>
2024-06-04 12:36:10 +0530

    Update Book.java (lab 10)

--- Book.java
+++ Book.java
@@ -1,6 +1,6 @@
 //Write a Java program to create a class called "Book" with attributes for title, author, and ISBN, and methods to add and remove books from a collection.
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 
 public class Book {
     // Declares private instance variables for title, author, and ISBN of the book
